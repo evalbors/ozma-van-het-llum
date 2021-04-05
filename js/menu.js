beffe2e5e43a91e5ab4a587dbe4a7a269ab003f6
evalbors <evalbors@gmail.com>
2021-04-05 13:21:49 +0200

    also adding section words

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+var header = document.getElementById("menu");
+var links = header.getElementsByClassName("link");
+for (var i = 0; i < btns.length; i++) {
+  links[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
