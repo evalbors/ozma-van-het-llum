10490940ffebdcb7be805ff41a7c41d4e2d9149b
evalbors <evalbors@gmail.com>
2021-04-05 13:45:42 +0200

    fixing menu active more or less

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 var header = document.getElementById("menu");
 var links = header.getElementsByClassName("link");
-for (var i = 0; i < btns.length; i++) {
+for (var i = 0; i < links.length; i++) {
   links[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(" active", "");
